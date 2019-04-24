c5ffc9f3300440a684e1c1135ddf2e2654bec7ce
Chris Cavanagh <chris.cavanagh@wdc.com>
2019-04-23 22:11:27 -0500

    Added File / Open SavedClips… menu option.

--- renderer.js
+++ renderer.js
@@ -1,167 +1,171 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
-const {shell} = require('electron')
-const os = require('os')
-const { dialog } = require('electron').remote
-const fs = require('fs')
-const path = require('path')
+const {shell, ipcRenderer} = require( "electron" )
+const os = require( "os" )
+const fs = require( "fs" )
+const path = require( "path" )
+const flatpickr = require( "flatpickr" )
+const regexHelpers = require( "./regex-helpers" )
+const { dialog } = require( "electron" ).remote
 
-var folders = dialog.showOpenDialog({ properties: ['openDirectory'] })
-var folderRegex = /(\d+)-(\d+)-(\d+)_(\d+)-(\d+)-(\d+)/g;
-var regex = /(\d+)-(\d+)-(\d+)_(\d+)-(\d+)-(.*).mp4/g;
-
-function groupBy(list, keyGetter)
+ipcRenderer.on( "folders", ( event, folders ) =>
 {
-    const map = new Map();
+	var folderRegex = /(\d+)-(\d+)-(\d+)_(\d+)-(\d+)-(\d+)/g;
+	var regex = /(\d+)-(\d+)-(\d+)_(\d+)-(\d+)-(.*).mp4/g;
 
-    list.forEach( (item) =>
-    {
-         const key = keyGetter(item);
-         const collection = map.get(key);
+	function groupBy( list, keyGetter )
+	{
+		const map = new Map();
 
-         if (!collection) map.set(key, [item]);
-         else collection.push(item);
-    });
+		list.forEach( ( item ) =>
+		{
+			const key = keyGetter( item );
+			const collection = map.get( key );
 
-    return map;
-}
+			if ( !collection ) map.set( key, [ item ] );
+			else collection.push(item);
+		});
 
-function extractDate( match, hasSeconds = false )
-{
-    var year = Number( match[ 1 ] )
-    var month = Number( match[ 2 ] ) - 1
-    var day = Number( match[ 3 ] )
-    var hour = Number( match[ 4 ] )
-    var minute = Number( match[ 5 ] )
-    var second = hasSeconds ? Number( match[ 6 ] ) : 0
+		return map;
+	}
 
-    return new Date( year, month, day, hour, minute, second )
-}
+	function extractDate( match, hasSeconds = false )
+	{
+		var year = Number( match[ 1 ] )
+		var month = Number( match[ 2 ] ) - 1
+		var day = Number( match[ 3 ] )
+		var hour = Number( match[ 4 ] )
+		var minute = Number( match[ 5 ] )
+		var second = hasSeconds ? Number( match[ 6 ] ) : 0
 
-function addElement( container, name, attributes )
-{
-    var element = document.createElement( name )
+		return new Date( year, month, day, hour, minute, second )
+	}
 
-    container.appendChild( element )
+	function addElement( container, name, attributes )
+	{
+		var element = document.createElement( name )
 
-    if ( attributes )
-    {
-        for ( var key in attributes ) element.setAttribute( key, attributes[ key ] )
-    }
+		container.appendChild( element )
 
-    return element
-}
+		if ( attributes )
+		{
+			for ( var key in attributes ) element.setAttribute( key, attributes[ key ] )
+		}
 
-var foldersElement = document.getElementById( "folders" )
-var baseFolder = folders[ 0 ]
+		return element
+	}
 
-fs.readdir( baseFolder, ( err, dir ) =>
-{
-    for ( var folder of dir )
-    {
-        var match = folderRegex.exec( folder )
-    
-        folderRegex.lastIndex = 0
-    
-        if ( match && match.length > 0 )
-        {
-            function addFolder( match )
-            {
-                var date = extractDate( match, hasSeconds = true )
-                var folderElement = addElement( foldersElement, "div", { class: "folder" } )
-                var folderTitle = addElement( folderElement, "div", { class: "title" } )
-                var folderPath = path.join( baseFolder, folder )
-        
-                folderTitle.innerText = date
-                folderTitle.addEventListener( "click", ( e, ev ) => loadFolder( folderPath, folderElement  ) )
-            }
+	var foldersElement = document.getElementById( "folders" )
+	var baseFolder = folders[ 0 ]
 
-            addFolder( match )
-        }
-    }    
+	fs.readdir( baseFolder, ( err, dir ) =>
+	{
+		for ( var folder of dir )
+		{
+			var match = folderRegex.exec( folder )
+		
+			folderRegex.lastIndex = 0
+		
+			if ( match && match.length > 0 )
+			{
+				function addFolder( match )
+				{
+					var date = extractDate( match, hasSeconds = true )
+					var folderElement = addElement( foldersElement, "div", { class: "folder" } )
+					var folderTitle = addElement( folderElement, "div", { class: "title" } )
+					var folderPath = path.join( baseFolder, folder )
+			
+					folderTitle.innerText = date
+					folderTitle.addEventListener( "click", ( e, ev ) => loadFolder( folderPath, folderElement  ) )
+				}
+
+				addFolder( match )
+			}
+		}    
+	} )
+
+	var selectedFolderElement = null
+
+	function loadFolder( folder, folderElement )
+	{
+		if ( selectedFolderElement ) selectedFolderElement.classList.remove( "selected" )
+
+		selectedFolderElement = folderElement
+		selectedFolderElement.classList.add( "selected" )
+
+		var element = document.getElementById( "videos" )
+
+		while ( element.firstChild )
+		{
+			element.removeChild( element.firstChild )
+		}
+
+		fs.readdir( folder, ( err, dir ) =>
+		{
+			var files = []
+
+			for ( var file of dir )
+			{
+				var match = regex.exec( file )
+
+				regex.lastIndex = 0
+
+				if ( match && match.length > 0 )
+				{
+					var date = extractDate( match )
+					var camera = match[ 6 ]
+					var filePath = path.join( folder, file )
+
+					files.push( { date: date, camera: camera, file: filePath } )
+				}
+			}
+
+			var grouped = groupBy( files, f => f.date.toString() )
+
+			for ( var [ dateTime, views ] of grouped )
+			{
+				function addVideos( views )
+				{
+					var div = addElement( element, "div", { class: "timespan" } )
+					var title = addElement( div, "h3", { class: "title" } )
+
+					title.innerText = dateTime
+
+					var videoContainer = addElement( div, "div", { class: "container" } )
+
+					function addVideo( className )
+					{
+						var column = addElement( videoContainer, "div", { class: "column" } )
+						var video = addElement( column, "video", { class: className } )
+
+						return video
+					}
+
+					var videos =
+					[
+						addVideo( "left_repeater" ),
+						addVideo( "front" ),
+						addVideo( "right_repeater" )
+					]
+
+					var end = addElement( videoContainer, "div", { class: "end" } )
+
+					for ( var view of views )
+					{
+						var video = videoContainer.getElementsByClassName( view.camera )
+
+						if ( video && video.length > 0 )
+						{
+							video[ 0 ].setAttribute( "src", view.file )
+						}
+					}
+
+					title.addEventListener( "click", ( e, ev ) => { for ( var v of videos ) { console.log( v ); v.play() } } )
+				}
+
+				addVideos( views )
+			}
+		} )
+	}
 } )
-
-var selectedFolderElement = null
-
-function loadFolder( folder, folderElement )
-{
-    if ( selectedFolderElement ) selectedFolderElement.classList.remove( "selected" )
-
-    selectedFolderElement = folderElement
-    selectedFolderElement.classList.add( "selected" )
-
-    var element = document.getElementById( "videos" )
-
-    while ( element.firstChild )
-    {
-        element.removeChild( element.firstChild )
-    }
-
-    fs.readdir( folder, ( err, dir ) =>
-    {
-        var files = []
-
-        for ( var file of dir )
-        {
-            var match = regex.exec( file )
-
-            regex.lastIndex = 0
-
-            if ( match && match.length > 0 )
-            {
-                var date = extractDate( match )
-                var camera = match[ 6 ]
-                var filePath = path.join( folder, file )
-
-                files.push( { date: date, camera: camera, file: filePath } )
-            }
-        }
-
-        var grouped = groupBy( files, f => f.date.toString() )
-
-        for ( var [ dateTime, views ] of grouped )
-        {
-            function addVideos( views )
-            {
-                var div = addElement( element, "div", { class: "timespan" } )
-                var title = addElement( div, "h3", { class: "title" } )
-
-                title.innerText = dateTime
-
-                var videoContainer = addElement( div, "div", { class: "container" } )
-
-                function addVideo( className )
-                {
-                    var column = addElement( videoContainer, "div", { class: "column" } )
-                    var video = addElement( column, "video", { class: className } )
-
-                    return video
-                }
-
-                var videos =
-                [
-                    addVideo( "left_repeater" ),
-                    addVideo( "front" ),
-                    addVideo( "right_repeater" )
-                ]
-
-                var end = addElement( videoContainer, "div", { class: "end" } )
-
-                for ( var view of views )
-                {
-                    var video = videoContainer.getElementsByClassName( view.camera )
-
-                    if ( video && video.length > 0 )
-                    {
-                        video[ 0 ].setAttribute( "src", view.file )
-                    }
-                }
-
-                title.addEventListener( "click", ( e, ev ) => { for ( var v of videos ) { console.log( v ); v.play() } } )
-            }
-
-            addVideos( views )
-        }
-    } )
-}
